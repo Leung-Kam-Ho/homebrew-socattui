148d242ad28b857fd35e1b89164a1d0667a5df46
Kam Ho <71874669+Leung-Kam-Ho@users.noreply.github.com>
2026-07-02 13:17:27 +0800

    Fix dependency installation with build_isolation: false

--- Formula/socattui.rb
+++ Formula/socattui.rb
@@ -10,9 +10,15 @@ class Socattui < Formula
   depends_on "python@3.12"
   depends_on "socat"
 
+  resource "hatchling" do
+    url "https://files.pythonhosted.org/packages/56/49/2797ec0ef88008a653a8867bb8d1e5c223cd2df8e40390dd5c6a0279cbc5/hatchling-1.30.1-py3-none-any.whl"
+    sha256 "161eacafb3c6f91526e92116d21426369f2c36e98c36a864f11a96345ad4ee31"
+  end
+
   def install
-    virtualenv_create(libexec, "python3.12")
-    virtualenv_install_with_resources
+    venv = virtualenv_create(libexec, "python3.12")
+    venv.pip_install resource("hatchling")
+    venv.pip_install buildpath, build_isolation: false
   end
 
   test do
